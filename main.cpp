99db084e2f38d752336659a479c36bed1f7c5641
Olti Gjoni <oltigjoni@Oltis-MacBook-Air.local>
2024-04-10 23:39:51 -0400

    report

--- main.cpp
+++ main.cpp
@@ -16,18 +16,18 @@ main.cpp
 #include "myAVLtree.hpp"
 
 int main() {
-    std::vector<int> myVec;
+    std::vector<int> myVec; //vector to store numbers
 
-    std::string filename = "input3.txt";
-    std::ifstream temp (filename);
+    std::string filename = "input3.txt"; //file to read from
+    std::ifstream temp (filename);//open file
 
-    std::string line;
-    std::string num;
+    std::string line; //line to read from file
+    std::string num;//number to read from line
 
-    std::string newline;
-    std::string num1;
+    std::string newline;//line to read from file
+    std::string num1;//number to read from line
 
-    int realnum;
+    int realnum;//number to add to vector
 
     while(std::getline(temp, line)) { //read file line by line
         std::stringstream temp2(line); //parse line
@@ -36,10 +36,10 @@ int main() {
         std::getline(temp2, num1, ' ');  
 
         if(num == "insert") { //if insert, add to vector
-            realnum = stoi(num1);
-            myVec.push_back(realnum);
+            realnum = stoi(num1);//convert string to int
+            myVec.push_back(realnum);//add to vector
         } else { 
-            myVec.push_back(-1);
+            myVec.push_back(-1);//add -1 to vector
         }
     }
     
